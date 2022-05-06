1322bb14a6f371a3bc08e6bc48f901e35d4542d0
JunxiLu <92705383+JunxiLu@users.noreply.github.com>
2022-05-06 12:25:02 +0800

    Practical 2 with challenge done

---
+++ app/src/main/java/sg/edu/np/mad/junxiprac2/MainActivity.java
@@ -0,0 +1,54 @@
+package sg.edu.np.mad.junxiprac2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        user student1 = new user("Junxi Lu",
+                "Second year IT student at Ngee Ann Polytechnic",
+                10219098,
+                false);
+
+        TextView nameTxt = findViewById(R.id.nameTxt);
+        nameTxt.setText(student1.name);
+
+        TextView descTxt = findViewById(R.id.descTxt);
+        descTxt.setText(student1.description);
+
+        Button followBtn = findViewById(R.id.followBtn);
+        if (student1.followed){
+            followBtn.setText(R.string.unfollow);
+        } else {
+            followBtn.setText(R.string.follow);
+        }
+
+        followBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button followBtn = findViewById(R.id.followBtn);
+                if (student1.followed){
+                    followBtn.setText(R.string.follow);
+                } else {
+                    followBtn.setText(R.string.unfollow);
+                }
+
+                //Update followed status accordingly
+                student1.followed = !student1.followed;
+            }
+        });
+    }
+}
